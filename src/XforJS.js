94dcb4bf2b359d5104c70dd01bd7ccb734be75bb
joseph <joseph@JHP>
2012-12-31 07:35:59 -0700

    Adding AbstractExpression.

--- src/XforJS.js
+++ src/XforJS.js
@@ -51,6 +51,7 @@ var XforJS = (function(){//change the name XforJS to anything you need.
    //INCLUDE productions/ProductionContext
 
    //INCLUDE productions/AbstractAssignment
+   //INCLUDE productions/AbstractExpression
    //INCLUDE productions/AbstractVariableDeclaration
 
    //INCLUDE productions/Program
---
+++ src/productions/AbstractExpression.js
@@ -0,0 +1,124 @@
+/*!
+ * Copyright 2012 Joseph Spencer.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information, visit http://SOMESITE
+ */
+
+
+/**
+ * @constructor
+ */
+function AbstractExpression(){}
+extend(AbstractExpression, Production);
+/**
+ * @const
+ * @type {string}
+ */
+AbstractExpression.prototype.name="AbstractExpression";
+
+AbstractExpression.prototype._hasOperator=false;
+AbstractExpression.prototype._hasValue=false;
+AbstractExpression.prototype._logicalNot=/^([!~]+)/;
+AbstractExpression.prototype._typeof=/^(typeof)(?=[\(\s])/;
+/**
+ * @override
+ * @param {CharWrapper} characters
+ * @param {ProductionContext} context
+ */
+AbstractExpression.prototype.execute=function(characters, context){
+   var output = this.getOutput();
+
+   characters.removeSpace();
+
+   if(characters.charAt(0) !== '}'){
+      if(this._hasValue === false || this._hasOperator){//Go to Value
+         this._hasOperator=false;
+         this._hasValue=true;
+         var match;
+         var negation;
+
+         //reserve this block for unary operators only
+         switch(characters.charAt(0)){
+         case '!':
+         case '~':
+            match = characters.match(this._logicalNot);
+            match.find();
+            negation = match.group(1);
+            characters.shift(negation.length);
+            characters.removeSpace();
+            output.add(negation);
+            break;
+         case 't':
+            match = characters.match(this._typeof);
+            if(match.find()){
+               characters.shift(match.group(1).length);
+               output.add("typeof");
+               characters.removeSpace();
+               if(characters.charAt(0) !== '('){
+                  output.add(" ");
+               }
+            }
+         }
+
+         if(characters.charAt(0) === '('){
+            characters.shift(1);
+            var parenthesizedExpressionOutput = new Output();
+            output.add(parenthesizedExpressionOutput);
+            context.addProduction(this.getParenthesizedExpression(parenthesizedExpressionOutput));
+         } else {
+            context.addProduction(this.getValue());
+         }
+         return;
+      } else if(this._hasValue){//Go to Operator
+         switch(characters.charAt(0)){
+         case ']':
+         case ')':
+            break;
+         default:
+            this._hasOperator=true;
+            this._hasValue=false;
+            context.addProduction(new Operator(output));
+            return;
+         }
+      }
+   }
+   if(this._hasValue && !this._hasOperator){
+      context.removeProduction();
+      return;
+   } else if(this._hasOperator){
+      throw "Unclosed Operator.";
+   }
+   throw "Empty Expression.";
+};
+/**
+ * Used to get the Value Production.
+ *
+ * @abstract
+ * @return {Production}
+ */
+AbstractExpression.prototype.getValue=function(){};
+
+/**
+ * Used when parenthesis are found.
+ * @abstract
+ * @param {Output} output
+ * @return {Production}
+ */
+AbstractExpression.prototype.getParenthesizedExpression=function(output){};
+/**
+ * @abstract
+ * @return {Output}
+ */
+AbstractExpression.prototype.getOutput=function(){};
