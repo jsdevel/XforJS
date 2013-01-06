05dd15eb0e4941622b7ef8e73b78d076a20301fd
joseph <joseph@JHP>
2013-01-06 00:49:47 -0700

    Getting an initial template together.

---
+++ bin/build.js
@@ -0,0 +1,29 @@
+/*!
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var fs = require('fs');
+var unpacked = fs.readFileSync('../index.unpacked.html', 'utf8');
+unpacked = buildFile(unpacked.replace(/(>)\s+(<)/g, "$1$2")).withPath("../").now();
+fs.writeFileSync('../index.html', unpacked, 'utf8');
+
+
+function buildFile(file, addExt){
+   var _path="";
+
+   return {
+      now:function(){
+         return file.replace(/^\s*?\/\/INCLUDE\s([^\n\r]+)/gm,
+            function(match,group1){
+               var filePath = _path+group1+(addExt?".js":"");
+               var fileContents = fs.readFileSync(filePath, 'utf8');
+
+               return fileContents.replace(/\/\*\*?!(?:(?!\*\/)[\s\S])*?\*\//g, '');
+            });
+      },
+      withPath:function(path){
+         _path = path;
+         return this;
+      }
+   };
+}
